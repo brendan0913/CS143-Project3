7ae152396e6255e1d948a3a8acec54aa42e0185a
Cole Strain <colestrain2000@gmail.com>
2021-11-02 20:31:23 -0700

    Log birth/founded information to console

--- convert.js
+++ convert.js
@@ -8,19 +8,38 @@ let data = JSON.parse(file)
 .laureates.forEach(laureate => {
     // get the id, givenName, and familyName of the laureate
     let id = laureate.id;
-    if (laureate.givenName) {
+    let person = laureate.givenName ? true : false;
+    if (person) {
         let givenName = laureate.givenName.en;
         let familyName = laureate.familyName ? laureate.familyName.en : null;
 
         // print the extracted information
         console.log(id + "\t" + givenName + (familyName ? ("\t" + familyName) : null));
-    } else if (laureate.orgName) {
+    } else {
         let orgName = laureate.orgName.en;
 
         // print the extracted information
         console.log(id + "\t" + orgName);
-    } else {
-        console.log("WTF did you do?");
-        return;
     }
+
+    // get the birth/founding date of the laureate
+    let birthDate, birthCity, birthCountry;
+    if (person && laureate.birth) {
+        birthDate = laureate.birth.date;
+        if (laureate.birth.place.city) {
+            birthCity = laureate.birth.place.city.en;
+        }
+        if (laureate.birth.place.country) {
+            birthCountry = laureate.birth.place.country.en;
+        }
+    } else if (laureate.founded) {
+        birthDate = laureate.founded.date;
+        if (laureate.founded.place.city) {
+            birthCity = laureate.founded.place.city.en;
+        }
+        if (laureate.founded.place.country) {
+            birthCountry = laureate.founded.place.country.en;
+        }
+    }
+    console.log((birthDate ? (birthDate + "\t") : null) + (birthCity ? (birthCity + "\t") : null) + (birthCountry ?? null));
 });
