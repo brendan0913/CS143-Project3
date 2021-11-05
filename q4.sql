0ce89f98cde7885d3e5a30dc353d741aa138d946
Brendan <bmango643@gmail.com>
2021-11-04 18:36:12 -0700

    Completed query 4

--- q4.sql
+++ q4.sql
@@ -4,3 +4,5 @@
 -- - Birth(lid, date, city, country) // Also used for organization foundings
 -- - Prize(nid, awardYear, category, sortOrder, affilIds)
 -- - Affiliation(affilId, name, city, country)
+
+SELECT DISTINCT COUNT(*) OVER() diff_locations FROM Affiliation WHERE name="University of California" GROUP BY city, country;
