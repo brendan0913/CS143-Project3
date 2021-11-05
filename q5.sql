10ec3ef1d6f5054e51a9b8a592cb98b2ce1573c1
Brendan <bmango643@gmail.com>
2021-11-04 18:41:27 -0700

    Completed query 5

--- q5.sql
+++ q5.sql
@@ -4,3 +4,7 @@
 -- - Birth(lid, date, city, country) // Also used for organization foundings
 -- - Prize(nid, awardYear, category, sortOrder, affilIds)
 -- - Affiliation(affilId, name, city, country)
+
+SELECT DISTINCT COUNT(*) OVER() org_prize_years FROM Prize 
+WHERE nid IN(SELECT nid FROM Laureate L, Organization O WHERE L.lid = O.lid ) 
+GROUP BY awardYear
