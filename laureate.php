ce60bc18b2ca7f876078384e1ec35fec44576c8d
Brendan <bmango643@gmail.com>
2021-11-05 13:16:09 -0700

    TODO: organization, nobel prizes, affiliation

--- laureate.php
+++ laureate.php
@@ -8,24 +8,81 @@ header('Content-Type: application/json');
 
 /*
    Send the following fake JSON as the result
-   {  "id": $id,
-      "givenName": { "en": "A. Michael" },
-      "familyName": { "en": "Spencer" },
-      "affiliations": [ "UCLA", "White House" ]
-   }
+    {
+        "id":"745",
+        "givenName":{ "en":"A. Michael" },
+        "familyName":{ "en":"Spence" },
+        "gender":"male",
+        "birth":{
+            "date":"1943-00-00",
+            "place":{
+                "city":{ "en":"Montclair, NJ" },
+                "country":{ "en":"USA" }
+            }
+        },
+        "nobelPrizes":[{
+            "awardYear":"2001",
+            "category":{ "en":"Economic Sciences" },
+            "sortOrder":"2",
+            "affiliations":[{
+                "name":{ "en":"Stanford University" },
+                "city":{ "en":"Stanford, CA" },
+                "country":{ "en":"USA" }
+            }]
+        }]
+    }
 */
+
+$db = new mysqli('localhost', 'cs143', '', 'class_db');
+if ($db->connect_errno > 0) die('Unable to connect to database [' . $db->connect_error . ']');
+
+$query = "SELECT * FROM Person WHERE lid = $id";
+$rs = $db->query($query);
+while ($row = $rs->fetch_assoc()) { 
+    $first = $row['givenName']; 
+    $last = $row['familyName'];
+    $gender = $row['gender']; 
+}
+$rs->free();
+
+$query = "SELECT * FROM Birth WHERE lid = $id";
+$rs = $db->query($query);
+while ($row = $rs->fetch_assoc()) { 
+    $date = $row['date']; 
+    $city = $row['city'];
+    $country = $row['country']; 
+}
+$rs->free();
+
 $output = (object) [
     "id" => strval($id),
     "givenName" => (object) [
-        "en" => "A. Michael"
+        "en" => $first
     ],
     "familyName" => (object) [
-        "en" => "Spencer"
+        "en" => $last
     ],
-    "affliations" => array(
-        "UCLA",
-        "White House"
-    )
+    "gender" => $gender,
+    "birth" => (object) [
+        "date" => $date,
+        "place" => (object) [
+            "city" => (object) [
+                "en" => $city
+            ],
+            "country" => (object) [
+                "en" => $country
+            ]
+        ]
+    ]
+    // "nobelPrizes" => array(
+    //     (object) [
+
+    //     ]
+    // )
+    // "affliations" => array(
+    //     "UCLA",
+    //     "White House"
+    // )
 ];
 echo json_encode($output);
 
