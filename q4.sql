26d10c4345067548defc729cb863959c9fd603b2
Brendan <bmango643@gmail.com>
2021-11-04 18:29:10 -0700

    Created q3-5 files

---
+++ q4.sql
@@ -0,0 +1,6 @@
+-- - Laureate(lid, nids)
+-- - Person(lid, givenName, familyName, gender)
+-- - Organization(lid, orgName)
+-- - Birth(lid, date, city, country) // Also used for organization foundings
+-- - Prize(nid, awardYear, category, sortOrder, affilIds)
+-- - Affiliation(affilId, name, city, country)
